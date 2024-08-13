1e872dd2d1d6cc87b88514a09ce03a02e91891ff
iamkiruu1723 <kiranlearnsdaily@gmail.com>
2024-08-13 17:54:57 +0530

    sampletesting1

---
+++ kiranProject/Practises/List Tupes.py
@@ -0,0 +1,11 @@
+Klist = ['banana', "cherry", "appleuu"]
+print(Klist)
+Klist.append("ornage")
+print(Klist)
+Klist.remove("banana")
+print(Klist)
+Klist.append("papayaa")
+print(Klist)
+
+print(Klist[3:])
+print(Klist[-1])
